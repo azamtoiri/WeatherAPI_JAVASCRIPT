e02c723f01606fa466b98e9a2f82a55be4ce1661
spike <azamtoiri@gmail.com>
2024-05-01 22:24:45 +0300

    test: add new latest tests
    * ADD 4 tests

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
+
 const BASE_URL = 'http://127.0.0.1:3000'; // Замените на адрес вашего сервера
 
 
@@ -6,13 +8,32 @@ const BASE_URL = 'http://127.0.0.1:3000'; // Замените на адрес в
 test('GET /', async () => {
     const response = await axios.get(BASE_URL);
     expect(response.status).toBe(200);
-    expect(response.data).toStrictEqual({"message": "hello"});
+
+    // Проверка наличия ожидаемых элементов или текста на странице
+    const $ = cheerio.load(response.data);
+    expect($('h1').length).toBeGreaterThan(0); // Пример: ожидаем наличие хотя бы одного заголовка h1
+    expect($('h1').text()).toBe('WEATHER API'); // Пример: ожидаемый текст заголовка h1
+
 });
 
 // Тест для проверки доступности эндпоинта /weather
-test('GET /weather', async () => {
+test('GET /weather TEST endpoint is available', async () => {
     const response = await axios.get(`${BASE_URL}/weather`);
     expect(response.status).toBe(200);
     expect(response.data).toBeDefined();
     expect(response.data.length).toBeGreaterThan(0);
+});
+
+// Тест для проверки эндпоинта /weather
+test('POST /weather TEST post request to the endpoint', async () => {
+    const response = await axios.post(`${BASE_URL}/weather`, { city: 'Moscow' });
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.data) && response.data.length > 0).toBe(true);
+});
+
+// GET TEST
+test('GET /weather TEST get request to the endpoint', async () => {
+    const response = await axios.get(`${BASE_URL}/weather`, { params: { city: 'Moscow' } });
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.data) && response.data.length > 0).toBe(true);
 });
